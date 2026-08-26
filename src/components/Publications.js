f8a981cc9f6a5066f5f6da9b3622415e52efc5e9
umesh-21 <umeshk@iitbhilai.ac.in>
2026-08-26 11:57:39 +0530

    updated publication

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -8,10 +8,39 @@ function Publications() {
   const publications = [
     {
       category: 'conf',
+      title: 'Doc-Protector: A Self-Healing Approach for Digital Documents',
+      authors: 'Umesh Kashyap, Raghav Borikar, Sudev Kumar Padhi, Sk. Subidh Ali',
+      venue: 'Security, Privacy, and Applied Cryptography Engineering'
+      
+    },
+    {
+      category: 'conf',
+      title: 'Doc-Protector: A Self-Healing Approach for Digital Documents',
+      authors: 'Sudev Kumar Padhi, Archana Tiwari, Umesh Kashyap, Sk. Subidh Ali',
+      venue: 'The 20th International Conference on Document Analysis and Recognition (ICDAR 2025)',
+      link: 'https://link.springer.com/article/10.1007/s41635-026-00186-0'
+    },
+    {
+      category: 'journal',
+      title: 'Generalize Adversarial Malware Detection',
+      authors: 'Umesh Kashyap, Ashish Vishwakarma, Sk. Subidh Ali',
+      venue: 'Journal of Hardwareand Systems Security',
+      link: 'https://link.springer.com/article/10.1007/s41635-026-00186-0'
+    },
+
+    {
+      category: 'conf',
+      title: 'Perceptually-Guided Adversarial Diffusion: Enhancing Visual Fidelity in Facial Privacy',
+      authors: 'Saurabh Nevase, Umesh Kashyap, Sk. Subidh Ali',
+      venue: 'International Conference on Computer Vision & Image Processing (CVIP 2025)'
+    },
+
+    {
+      category: 'conf',
       title: 'GAN-Invert: Unveiling Vulnerabilities in Privacy-Preserving Facial Transformations',
       authors: 'Umesh Kashyap, Sk. Subidh Ali',
       venue: 'Privacy Enhancing Technologies Symposium (PETs 2026) Issue 2',
-      link: 'https://petsymposium.org/2026/paperlist.php'
+      link: 'https://doi.org/10.56553/popets-2026-0037'
     },
     {
       category: 'conf',
